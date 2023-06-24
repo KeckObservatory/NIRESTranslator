422ed23239298f76207618e12a88d82f2a31a46c
Tyler Tucker <tylertucker202@gmail.com>
2023-06-23 16:18:41 -1000

    writing shared functions and their helpers

--- nires/NIRESTranslatorFunction.py
+++ nires/NIRESTranslatorFunction.py
@@ -11,6 +11,45 @@ except:
 
 class NIRESTranslatorFunction(TranslatorModuleFunction):
 
+    @classmethod
+    def determine_nires_service(cls, sv):
+        """determines if use spectrograph (nsds) or imager server (nids)
+
+        Args:
+            sv (int): spec 's' or imager 'v'
+
+        Returns:
+            str:  
+        """
+        if sv == 's':
+            service = 'nsds'
+        elif sv == 'v':
+            service = 'nids'
+        else:
+            raise(f"input variable sv={sv} not 's' or 'v'")
+        return service
+
+    @classmethod
+    def minimum_integration_time(cls, sv):
+        """Calculates the minimum integration time for the NIRES imaging detector.
+
+        Args:
+            sv (int): spec 's' or imager 'v'
+
+        Returns:
+            int: minimum integration time for NIRES 
+        """        
+        service = cls.determine_nires_service(sv)
+        sampmode = ktl.read(service, 'sampmode')
+        readtime = ktl.read(service, 'readtime')
+        if sampmode==1 | sampmode==2:
+            nsamp = ktl.read(service, 'numreads')
+        else:
+            nsamp = 1
+        minTime = nsamp * readtime
+        return minTime
+        
+
     def _cfg_location(cls, args):
         """
         Return the fullpath + filename of default configuration file.
--- nires/shared/set_detector_configuration.py
+++ nires/shared/set_detector_configuration.py
@@ -1,3 +1,4 @@
+import ktl
 """
 Sets the following detector parameters:
 - integration time
@@ -23,6 +24,127 @@ from NIRESTranslatorFunction import NIRESTranslatorFunction
 
 class SetDetectorConfig(NIRESTranslatorFunction):
 
+    @classmethod
+    def set_integration_time(cls, requestTime, sv, logger):
+        """Sets the integration time to n seconds for either the NIRES spectrographic or imager detector.
+
+        Args:
+            requestTime (int): time in seconds.
+            sv (int): spec 's' or imager 'v'
+            logger (class): Logger object
+        """
+        minimumTime = cls.minimum_integration_time()
+        timeTooSmall = requestTime-minimumTime < 0
+        
+        time = minimumTime if (timeTooSmall) else requestTime
+        if timeTooSmall: logger.debug(f'Request for integration less than the minimum {minimumTime}')
+        service = cls.determine_nires_service(sv) 
+        ktl.write(service, 'itime', time, wait=True, timeout=2)
+        msg = f'Integration time set to {time}'
+        logger.info(msg)
+
+    @classmethod
+    def set_coadd(cls, nCoadd, sv, logger):
+        """will configure the NIRES spec/imaging server to 
+        coadd n integrations into one frame.
+
+        Args:
+            n (int): number of coadds 
+            sv (int): spec 's' or imager 'v'
+            logger (class): Logger object
+        """
+
+        if nCoadd<1:
+            nCoadd=1
+            logger.info('Attempt to set coadds to zero; coadds will be set to 1.')
+        service = cls.determine_nires_service(sv) 
+        ktl.write(service, 'coadds', nCoadd, wait=True, timeout=2)
+
+    @classmethod
+    def set_readout_mode(cls, readoutMode, sv, logger, nSamp=None):
+        """For the NIRES imaging/spec detector, sets the readout mode to
+        one of four types. If MCDS is chosen, a second parameter can be used to set
+        the number of samples. 
+
+        Args:
+            readoutMode (int): Known modes are:
+                                1 (or "UTR") Up The Ramp sampling 
+                                2 (or "PCDS") Pseudo Correlated Double Sampling
+                                3 (or "MCDS") Multiple-read Correlated Double (Fowler) Sampling
+                                4 (or "single") Single exposure
+            sv (int): spec 's' or imager 'v'
+            logger (class): Logger object
+        """
+
+        if readoutMode == 1:
+            msg = "Sampling mode = 1 (up the Ramp sampling)"
+        elif readoutMode == 2:
+            msg = "Sampling mode = 2 (pseudo-correlated double sampling)"
+        elif readoutMode == 3:
+            msg = "Sampling mode = 3 (multiple correlated double (Fowler) sampling)"
+        elif readoutMode == 4:
+            msg = "Sampling mode = 4 (single sample)"
+        else:
+            logger.warn("Do not understand sampling mode. Not going to set.")
+            return
+            
+
+        logger.info(msg)
+        service = cls.determine_nires_service(sv) 
+        ktl.write(service, 'sampmode', readoutMode, wait=True, timeout=2)
+
+        if readoutMode==3 and nSamp:
+            logger.info(f'Setting number of Fowler mode samples to {nSamp}')
+            cls.set_number_of_samples(nSamp, sv, logger, readoutMode)
+
+        cls.check_integration_time(sv, logger)
+    
+    @classmethod
+    def check_integration_time(cls, sv, logger):
+        """Compare integration time with minimum time and reset if nessessary
+
+        Args:
+            sv (int): spec 's' or imager 'v'
+            logger (class): Logger object
+        """
+        service = cls.determine_nires_service(sv) 
+        integrationTime = ktl.read(service, 'itime')
+        minimumTime = cls.minimum_integration_time(sv)
+        iTimeTooSmall = integrationTime-minimumTime < 0
+        if iTimeTooSmall:
+            logger.debug("setting itime to zero")
+            cls.set_integration_time(minimumTime, sv, logger)
+
+    @classmethod
+    def set_number_of_samples(cls, nSamp, sv, logger, readoutMode=None):
+        """Sets the number of MCDS (multiple double-correlated sampling) mode
+        reads to nSamp for the NIRES spectrograph/imager server.
+
+        Args:
+            nSamp (int): number of Fowler Samples 
+            sv (int): spec 's' or imager 'v'
+            logger (class): Logger object
+            readoutMode (int): Optional. Use if you dont want to read sampmode. 
+                               Known modes are:
+                                1 (or "UTR") Up The Ramp sampling 
+                                2 (or "PCDS") Pseudo Correlated Double Sampling
+                                3 (or "MCDS") Multiple-read Correlated Double (Fowler) Sampling
+                                4 (or "single") Single exposure
+        """        
+
+        service = cls.determine_nires_service(sv) 
+
+        if not readoutMode:
+            readoutMode = ktl.read(service, 'sampmode')
+
+        if readoutMode==3: # Fowler mode
+            ktl.write(service, 'numfs', nSamp)
+        else:
+            ktl.write(service, 'numreads', nSamp)
+        # Check to make sure minumum 
+        # integration time is consitant with old
+        cls.check_integration_time(sv, logger) 
+             
     @classmethod
     def pre_condition(cls, args, logger, cfg):
         pass
