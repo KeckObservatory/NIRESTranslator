785535caa1462f3efee3963a948b984f26025455
Max Brodheim <max.brodheim@gmail.com>
2023-07-19 15:35:19 -1000

    fill out snapi

--- nires/imager/snap_image.py
+++ nires/imager/snap_image.py
@@ -7,9 +7,17 @@ snapi
 
 from astropy.io import fits
 import os
+import shutil
 from datetime import datetime
 
 from NIRESTranslatorFunction import NIRESTranslatorFunction
+from nires.shared.take_exposures import TakeExposures
+from nires.shared.wait_for_exposure import WaitForExposure
+
+try:
+    import ktl
+except ImportError:
+    ktl = ''
 
 class SnapImage(NIRESTranslatorFunction):
 
@@ -23,22 +31,62 @@ class SnapImage(NIRESTranslatorFunction):
 
     @classmethod
     def perform(cls, args, logger, cfg):
+
         # Take object frame
+        cls._write_to_ktl('nids', 'comment', 'Object image for snapi', logger, cfg)
+        TakeExposures.execute({'nFrames' : 1, 'sv' : 'v'})
+        WaitForExposure.execute({'sv' : 'v'})
         # Get lastfile
+        object_file = ktl.read('nids', 'lastfile')
+        shutil.copyfile(object_file, cfg['ob_keys']['object_temp_path'])
+        os.chmod(cfg['ob_keys']['object_temp_path'], 666)
+
         # Offset
+
+        nod_east = ktl.read('nires', 'node')
+        nod_north = ktl.read('nires', 'nodn')
+        cls._write_to_ktl('dcs', 'raoff', nod_east, logger, cfg)
+        cls._write_to_ktl('dcs', 'decoff', nod_north, logger, cfg)
+        cls._write_to_ktl('dcs', 'rel2curr', 't', logger, cfg)
+        cls.wait_for_tel(cfg, logger)
+
+
         # Take sky frame
+        cls._write_to_ktl('nids', 'comment', 'Sky image for snapi', logger, cfg)
+        TakeExposures.execute({'nFrames' : 1, 'sv' : 'v'})
+        WaitForExposure.execute({'sv' : 'v'})
         # Get lastfile
-        # Use the lastfiles to generate subtracted image, save to tempdri
-        pass
+        sky_file = ktl.read('nids', 'lastfile')
+        shutil.copyfile(sky_file, cfg['ob_keys']['sky_temp_path'])
+        os.chmod(cfg['ob_keys']['sky_temp_path'], 666)
+        # Use the lastfiles to generate subtracted image, save to tempdr
+        cls.subtract_fits(cfg['ob_keys']['object_temp_path'], cfg['ob_keys']['sky_temp_path'])
+
+        # move back
+        nod_east = ktl.read('nires', 'node') * -1
+        nod_north = ktl.read('nires', 'nodn') * -1
+        cls._write_to_ktl('dcs', 'raoff', nod_east, logger, cfg)
+        cls._write_to_ktl('dcs', 'decoff', nod_north, logger, cfg)
+        cls._write_to_ktl('dcs', 'rel2curr', 't', logger, cfg)
+        cls.wait_for_tel(cfg, logger)
 
     @classmethod
     def post_condition(cls, args, logger, cfg):
         pass
 
-    def subtract_fits(cls, file1, file2, outfile):
+    def display_subtracted_image(cls, file1, file2, cfg, logger, outfile=None):
+
+        
+        if outfile == None:
+            outfile = cfg['ob_keys']['default_snapi']
+
         with fits.open(file1) as f1:
             with fits.open(file2) as f2:
                 outdata = f1[0].data - f2[0].data
                 hdu = fits.PrimaryHDU(outdata)
                 hdu.writeto(outfile, overwrite=True)
+                os.chmod(outfile, 666)
+                cls.send_fits_to_display(outfile, logger, cfg)
+
+        
 
