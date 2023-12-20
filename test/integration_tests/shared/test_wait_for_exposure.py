a46abf9bb21b26d177b6cfff1ce332f806843280
Tyler Tucker <tylertucker202@gmail.com>
2023-12-20 10:44:43 -1000

    test waitfor exposure

---
+++ test/integration_tests/shared/test_wait_for_exposure.py
@@ -0,0 +1,49 @@
+from nires.shared.wait_for_exposure import WaitForExposure as wfe 
+from nires.shared.set_detector_configuration import SetDetectorConfig as sdc
+import unittest
+from unittest.mock import Mock, patch, MagicMock
+import ktl
+import os
+
+
+def ktl_side_effects(service, value):
+    if value == 'itime': return 1
+    if value == 'sampmode': return 4
+    if value == 'numreads': return 1
+    if value == 'coadds': return 1
+    if value == 'readtime': return 1
+    if value == 'readtime': return 1
+    if value == 'imagedone': return 1
+    return 0
+
+def logger_side_effect(msg):
+    print(msg)
+
+class TestTakeTests(unittest.TestCase):
+
+    def setUp(self):
+        ktl.read = Mock()
+        ktl.read.side_effect = ktl_side_effects
+        self.logger = MagicMock(side_effect=logger_side_effect)
+        self.logger.info.side_effect = logger_side_effect
+        self.logger.debug.side_effect = logger_side_effect
+        self.cfg = {
+            'ob_keys': {
+                'n_read_padding': 1.5,
+                'extra_wait': 1,
+                'ktl_wait': False,
+                'ktl_timeout': 2,
+            },
+            'operation_mode': {
+                'operation_mode': 'operational'
+            }
+        }
+    
+    def test_wait_for_exposure(self):
+        args = {}
+        args['sv'] = 'v'
+        wfe.execute(args, logger=self.logger, cfg=self.cfg)
+
+
+if __name__ == "__main__":
+    unittest.main()
